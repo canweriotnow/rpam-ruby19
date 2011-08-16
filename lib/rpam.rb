639ac9bc5c46cc1c231f9cef0b07ca52121966a5
Fred <mail@fredemmott.co.uk>
2011-08-16 22:43:33 +0000

    Do not require the Rpam module to be included.
    - It still works as before if you do
    - Can also be called as:
      - Rpam.auth()
      - Rpam.authpam()

--- lib/rpam.rb
+++ lib/rpam.rb
@@ -1,10 +1,18 @@
 require 'rpam_ext'
 
 module Rpam
-  def authpam user, password, options = {}
-    options = options.dup
-    options[:service] ||= 'rpam' 
+  def authpam *args
+    ::Rpam.auth(*args)
+  end
 
-    Rpam::Ext.authpam(user, password, options[:service])
+  class <<self
+    def auth user, password, options = {}
+      options = options.dup
+      options[:service] ||= 'rpam'
+
+      Rpam::Ext.authpam(user, password, options[:service])
+    end
+
+    alias :authpam :auth
   end
 end
